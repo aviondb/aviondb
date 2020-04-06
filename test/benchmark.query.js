9c06dab67ecf999a3faad237c30d850350aeee8b
vaultec81 <47548474+vaultec81@users.noreply.github.com>
2020-04-05 19:14:10 -0700

    Add benchmarks

---
+++ test/benchmark.query.js
@@ -0,0 +1,85 @@
+'use strict'
+
+const IPFS = require('ipfs')
+const IPFSRepo = require('ipfs-repo')
+const DatastoreLevel = require('datastore-level')
+const OrbitDB = require('orbit-db')
+const Crypto = require('crypto')
+
+// Metrics
+let totalQueries = 0
+let seconds = 0
+let queriesPerSecond = 0
+let lastTenSeconds = 0
+let numberOfEntries = 40000;
+
+// Main loop
+const queryLoop = async (db) => {
+  
+  await db.findOne({nombre: "kim"})
+  totalQueries ++
+  lastTenSeconds ++
+  queriesPerSecond ++
+  setImmediate(() => queryLoop(db))
+}
+
+// Start
+console.log("Starting IPFS daemon...")
+
+const repoConf = {
+  storageBackends: {
+    blocks: DatastoreLevel,
+  },
+}
+
+const ipfs = new IPFS({
+  repo: new IPFSRepo('./orbitdb/benchmarks/ipfs', repoConf),
+  start: false,
+  EXPERIMENTAL: {
+    sharding: false,
+    dht: false,
+  },
+})
+
+ipfs.on('error', (err) => console.error(err))
+
+ipfs.on('ready', async () => {
+  const run = async () => {
+    try {
+      OrbitDB.addDatabaseType("ipfsdb.collection", require('../src/Collection'))
+      const orbit = await OrbitDB.createInstance(ipfs,{ directory: './orbitdb/benchmarks' })
+
+      const db = await orbit.create('orbit-db.benchmark', "ipfsdb.collection", {
+        replicate: false,
+        overwrite: true
+      })
+
+      console.log(`Creating ${numberOfEntries} documents for querying. Stand by! `)
+      for(var x = 0; x < numberOfEntries; x++) {
+        await db.insertOne({
+          id: Crypto.randomBytes(6).toString("base64"),
+          nombre: "kim"
+        })
+      }
+      // Metrics output
+      setInterval(() => {
+        seconds ++
+        if(seconds % 10 === 0) {
+          console.log(`--> Average of ${lastTenSeconds/10} q/s in the last 10 seconds`)
+          if(lastTenSeconds === 0)
+            throw new Error("Problems!")
+          lastTenSeconds = 0
+        }
+        console.log(`${queriesPerSecond} queries per second, ${totalQueries} queries in ${seconds} seconds (Oplog: ${db._oplog.length})`)
+        queriesPerSecond = 0
+      }, 1000)
+      // Start the main loop
+      queryLoop(db)
+    } catch (e) {
+      console.log(e)
+      process.exit(1)
+    }
+  }
+
+  run()
+})
