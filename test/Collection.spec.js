1767309131cb593a021c9f209f09727cdb706c5f
vasa <vasa@signy.io>
2020-04-10 05:57:11 +0530

    test: add tests
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@dappkit.io>

--- test/Collection.spec.js
+++ test/Collection.spec.js
@@ -3,6 +3,7 @@ const Cache = require('orbit-db-cache')
 const Keystore = require('orbit-db-keystore')
 const IdentityProvider = require('orbit-db-identity-provider')
 const assert = require('assert')
+const ObjectId = require("bson-objectid")
 
 var DefaultOptions = {};
 // Test utils
@@ -49,16 +50,128 @@ describe("Collection", function () {
     })
     afterEach(async () => {
         await store.drop()
+        const address = 'test-address'
+        const cache = new Cache(cacheStore)
+        const options = Object.assign({}, DefaultOptions, { cache })
+        store = new Store(ipfs, testIdentity, address, options)
         await cacheStore.open()
         await identityStore.open()
     })
     //TODO: Add tests 
-    it("InsertOne", async () => {
-        await store.insertOne({ name: "kim", age: 35 })
-        var result = await store.findOne({name:"kim"})
+    it("Insert", async () => {
+        await store.insert([{ name: "kim", age: 35 }])
+        var result = await store.findOne({ name: "kim" })
         assert.strictEqual(typeof result, "object")
         assert.strictEqual(result.age, 35);
         assert.strictEqual(result.name, "kim");
+    })
+    it("InsertOne", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        var result = await store.findOne({ name: "kim" })
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 35);
+        assert.strictEqual(result.name, "kim");
+    })
+    it("Find", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        var result = await store.find({ name: "kim" })
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result[0].age, 35);
+        assert.strictEqual(result[0].name, "kim");
+    })
+    it("FindOne", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        var result = await store.findOne({ name: "kim" })
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 35);
+        assert.strictEqual(result.name, "kim");
+    })
+    /* it("FindOneAndUpdate", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        var result = await store.findOneAndUpdate({ name: "kim" }, { $set: { name: "vasa", age: 22 } })
+        console.log(result)
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 22);
+        assert.strictEqual(result.name, "vasa");
+    }) */
+    it("FindOneAndDelete", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        var result = await store.findOneAndDelete({ name: "kim" })
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 35);
+        assert.strictEqual(result.name, "kim");
+    })
+    it("FindById", async () => {
+        await store.insertOne({ _id: "54495ad94c934721ede76d90" ,name: "kim", age: 35 })
+        var result = await store.findById("54495ad94c934721ede76d90")
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 35);
+        assert.strictEqual(result.name, "kim");
+    })
+    it("FindByIdAndDelete", async () => {
+        await store.insertOne({ _id: "54495ad94c934721ede76d90" ,name: "kim", age: 35 })
+        var result = await store.findByIdAndDelete("54495ad94c934721ede76d90")
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 35);
+        assert.strictEqual(result.name, "kim");
+    })
+    /* it("FindByIdAndUpdate", async () => {
+        await store.insertOne({ _id: "54495ad94c934721ede76d90" ,name: "kim", age: 35 })
+        var result = await store.findByIdAndUpdate("54495ad94c934721ede76d90")
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 35);
+        assert.strictEqual(result.name, "kim");
+    }) */
+    /* it("Update", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        var result = await store.update("54495ad94c934721ede76d90")
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 35);
+        assert.strictEqual(result.name, "kim");
+    }) */
+    /* it("UpdateOne", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        var result = await store.updateOne("54495ad94c934721ede76d90")
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 35);
+        assert.strictEqual(result.name, "kim");
+    }) */
+    /* it("UpdateMany", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        var result = await store.updateMany("54495ad94c934721ede76d90")
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 35);
+        assert.strictEqual(result.name, "kim");
+    }) */
+    it("DeleteOne", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        var result = await store.deleteOne({ name: "kim" })
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result.age, 35);
+        assert.strictEqual(result.name, "kim");
+    })
+    it("DeleteMany", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        var result = await store.deleteMany({ name: "kim" })
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result[0].age, 35);
+        assert.strictEqual(result[0].name, "kim");
+    })
+    it("Distinct", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        await store.insertOne({ name: "vasa", age: 22 })
+        var result = await store.distinct("name")
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result[0], "kim");
+        assert.strictEqual(result[1], "vasa");
+    })
+    it("Distinct", async () => {
+        await store.insertOne({ name: "kim", age: 35 })
+        await store.insertOne({ name: "vasa", age: 22 })
+        var result = await store.distinct("name")
+        assert.strictEqual(typeof result, "object")
+        assert.strictEqual(result[0], "kim");
+        assert.strictEqual(result[1], "vasa");
     })
     it("Head syncing", async () => {
         //NOTE: notice how there is two inserts. That is because entries aren't immediately added to the oplog. store.getHeadHash() will return null if there is no oplog entries. 
