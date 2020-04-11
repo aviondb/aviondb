75ffee670a09947a2eaca4934fbd45ae2b0c5517
vasa <vasa@signy.io>
2020-04-12 04:09:47 +0530

    test: add ListCollections test
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@dappkit.io>

--- test/Store.spec.js
+++ test/Store.spec.js
@@ -81,4 +81,29 @@ describe("DB", function () {
     it("Drop Collection", async () => {
         //TODO: Create test here
     })
+    it("List Collections", async () => { 
+        await ipfs.ready
+        var collection = await store.createCollection("Users")
+        await collection.insertOne({
+            name: "vasa"
+        })
+        var collections = store.listCollections()
+        assert.strictEqual(arraysEqual(collections, ['Accounts', 'Users']), true)
+    })
 })
+
+const arraysEqual = (a, b) => {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+  // Please note that calling sort on an array will modify that array.
+  // you might want to clone your array first.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
