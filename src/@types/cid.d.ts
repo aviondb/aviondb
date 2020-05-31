d50448f1e6e198bc53020996e09601b2fba5fe4d
vasa <vasa@signy.io>
2020-05-31 06:24:11 +0530

    refactor: update CID type definition
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@dappkit.io>

--- src/@types/cid.d.ts
+++ src/@types/cid.d.ts
@@ -1,26 +1,118 @@
-export as namespace CID;
-export = CID;
-
-type Buffer = any;
-
-interface SerializedCID {
-  codec: string;
-  version: number;
-  multihash: Buffer;
-}
-
+/**
+ * Class representing a CID `<mbase><version><mcodec><mhash>`
+ * , as defined in [ipld/cid](https://github.com/multiformats/cid).
+ */
 declare class CID {
-  constructor(version: number, codec: string, multihash: Buffer);
-  static isCID(other: any): boolean;
-  static validateCID(other: any): void;
-  codec: string;
+  /**
+   * Create a new CID.
+   *
+   * The algorithm for argument input is roughly:
+   * ```
+   * if (cid)
+   *   -> create a copy
+   * else if (str)
+   *   if (1st char is on multibase table) -> CID String
+   *   else -> bs58 encoded multihash
+   * else if (Buffer)
+   *   if (1st byte is 0 or 1) -> CID
+   *   else -> multihash
+   * else if (Number)
+   *   -> construct CID by parts
+   * ```
+   *
+   * @example
+   * new CID(<version>, <codec>, <multihash>, <multibaseName>)
+   * new CID(<cidStr>)
+   * new CID(<cid.buffer>)
+   * new CID(<multihash>)
+   * new CID(<bs58 encoded multihash>)
+   * new CID(<cid>)
+   */
+  constructor(
+    version: 0 | 1,
+    codec: string,
+    multhash: Buffer,
+    multibaseName?: string
+  );
+  constructor(cid: CID);
+  constructor(str: string);
+  constructor(buf: Buffer);
+
+  /**
+   * The version of the CID.
+   */
   version: number;
+
+  /**
+   * The codec of the CID.
+   */
+  codec: string;
+
+  /**
+   * The multihash of the CID.
+   */
   multihash: Buffer;
-  buffer: Buffer;
-  prefix: Buffer;
+
+  /**
+   * Multibase name as string.
+   */
+  multibaseName: string;
+
+  /**
+   * The CID as a `Buffer`
+   */
+  readonly buffer: Buffer;
+
+  /**
+   * The prefix of the CID.
+   */
+  readonly prefix: Buffer;
+
+  /**
+   * Convert to a CID of version `0`.
+   */
   toV0(): CID;
+
+  /**
+   * Convert to a CID of version `1`.
+   */
   toV1(): CID;
+
+  /**
+   * Encode the CID into a string.
+   *
+   * @param base Base encoding to use.
+   */
   toBaseEncodedString(base?: string): string;
-  toJSON(): SerializedCID;
-  equals(other: CID): boolean;
+
+  /**
+   * Encode the CID into a string.
+   */
+  toString(base?: string): string;
+
+  /**
+   * Serialize to a plain object.
+   */
+  toJSON(): { codec: string; version: 0 | 1; hash: Buffer };
+
+  /**
+   * Compare equality with another CID.
+   *
+   * @param other The other CID.
+   */
+  equals(other: any): boolean;
+
+  /**
+   * Test if the given input is a valid CID object.
+   * Throws if it is not.
+   *
+   * @param other The other CID.
+   */
+  static validateCID(other: any): void;
+
+  static isCID(mixed: any): boolean;
+
+  static codecs: Record<string, number>;
 }
+
+export = CID;
